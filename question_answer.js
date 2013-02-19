e9c594e92b53068857ac767eb41037ceb6e24b2d
Sagar <sagar.patil.smp@gmail.com>
2013-02-19 18:11:16 +0530

    my patshala support page final deployment

--- question_answer.js
+++ question_answer.js
@@ -15,4 +15,27 @@ $(document).ready(function () {
 				var class_name = "table."+$(this).attr('data-class');
 				$(class_name).slideToggle();
 		})
+
+		var changeTooltipPosition = function(event) {
+	  var tooltipX = event.pageX - 8;
+	  var tooltipY = event.pageY + 8;
+	  $('div.tooltip').css({top: tooltipY, left: tooltipX});
+	};
+ 
+	var showTooltip = function(event) {
+	  $('div.tooltip').remove();
+	  $('<div class="tooltip">Click on the question to see the answer.</div>')
+            .appendTo('body');
+	  changeTooltipPosition(event);
+	};
+ 
+	var hideTooltip = function() {
+	   $('div.tooltip').remove();
+	};
+ 
+	$("ol#hint").bind({
+	   mousemove : changeTooltipPosition,
+	   mouseenter : showTooltip,
+	   mouseleave: hideTooltip
+	});
 	})
